80de3d6da135b10dcb5b7e2202142ca4b9b2e717
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-07-11 17:12:41 -0400

    modify program.cs

--- comp123-lesson9b/Program.cs
+++ comp123-lesson9b/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/* Name: Aldrin Fernandez
+ * Date:July , 2017
+ * Description: Demo for lesson 9
+ * */
 namespace comp123_lesson9b
 {
     class Program
