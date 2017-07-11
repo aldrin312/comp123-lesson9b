185dcb5529c96f810c29d2c499fd63ba43501555
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-07-11 17:35:54 -0400

    added superhuman class

---
+++ comp123-lesson9b/SuperHuman.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Name: Aldrin Fernandez
+ * Date:July , 2017
+ * Description: THis is the superhuman class that  extend the human abstrac class
+ *  Version 0.1 - Create superhuman abstract class
+ */
+
+namespace comp123_lesson9b
+{
+    /// <summary>
+    /// THis is the SuperHuman Class
+    /// </summary>
+    public class SuperHuman : Human
+    {
+
+        //PRIVATE INTANCE VARIABLES(FIELDS)
+
+
+
+        //PUBLIC PROTPERTIES
+
+        //CONSTRUCTOR----------------------------------------------------------------
+        public SuperHuman(string name)
+            : base(name)
+        {
+
+        }
+
+        //PUBLIC METHIDS
+
+        //PRIVATE METHODS
+    }
+}
