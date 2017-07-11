128d18059d6bf5f28fc8f455d47e82277567a232
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-07-11 17:29:28 -0400

    added human abstract class

---
+++ comp123-lesson9b/Human.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Name: Aldrin Fernandez
+ * Date:July , 2017
+ * Description: THis is the human abstract class
+ *  Version 0.1 - Create human abstract class
+ */
+namespace comp123_lesson9b
+{
+    /// <summary>
+    ///  This is the human abstract class
+    /// </summary>
+    public abstract class Human
+    {
+
+        //PRIVATE INTANCE VARIABLES(FIELDS)
+        private string _name;
+
+
+        //PUBLIC PROTPERTIES
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+        //CONSTRUCTOR----------------------------------------------------------------
+
+            /// <summary>
+            /// this is the main constructor
+            /// it takes pme [ara,eter - name (string)
+            /// </summary>
+            /// <param name="name"></param>
+        public Human(string name)
+        {
+            this.Name = name;
+        }
+        //PUBLIC METHIDS
+
+        //PRIVATE METHODS
+    }
+}
--- comp123-lesson9b/Program.cs
+++ comp123-lesson9b/Program.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 /* Name: Aldrin Fernandez
  * Date:July , 2017
  * Description: Demo for lesson 9
- * */
+ *  Version 0.1 - Create Program class
+ */
 namespace comp123_lesson9b
 {
     class Program
