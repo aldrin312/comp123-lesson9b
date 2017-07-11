0b296e796defbb76963ee6295a5b6181908de852
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-07-11 17:47:33 -0400

    added _initialize method to superhuman class

--- comp123-lesson9b/SuperHuman.cs
+++ comp123-lesson9b/SuperHuman.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 /* Name: Aldrin Fernandez
  * Date:July , 2017
  * Description: THis is the superhuman class that  extend the human abstrac class
- *  Version 0.1 - Create superhuman abstract class
+ *  Version 0.2 - Added the initialize method
  */
 
 namespace comp123_lesson9b
@@ -20,18 +20,33 @@ namespace comp123_lesson9b
 
         //PRIVATE INTANCE VARIABLES(FIELDS)
 
+        private List<Power> _powers;
 
 
         //PUBLIC PROTPERTIES
 
         //CONSTRUCTOR----------------------------------------------------------------
+
+        /// <summary>
+        /// This is the main constructor for super human class
+        /// </summary>
+        /// <param name="name"></param>
         public SuperHuman(string name)
             : base(name)
         {
-
+            this._initialize();
         }
 
         //PUBLIC METHIDS
+
+            /// <summary>
+            /// this initialize, instantiate and assign values to class
+            /// </summary>
+        private void _initialize()
+        {
+            this._powers = new List<Power>();
+        }
+
 
         //PRIVATE METHODS
     }
