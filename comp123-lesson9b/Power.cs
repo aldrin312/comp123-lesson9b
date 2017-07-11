6acae8f58bc2b682cdeaa8d199ee0ec830582f09
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-07-11 17:40:35 -0400

    added power class

---
+++ comp123-lesson9b/Power.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    /* Name: Aldrin Fernandez
+     * Date:July , 2017
+     * Description: THis is the human abstract class
+     *  Version 0.1 - Create power class
+     */
+namespace comp123_lesson9b
+{
+    /// <summary>
+    /// This is a Power Class that will be used as a Data type
+    /// </summary>
+    class Power
+    {
+        //PRIVATE INTANCE VARIABLES
+
+        //PUBLIC PROTPERTIES
+        public string Name { get; set; }
+        public int Rank { get; set; }
+        //CONSTRUCTOR----------------------------------------------------------------
+
+        //PUBLIC METHIDS
+
+        //PRIVATE METHODS
+    }
+}
