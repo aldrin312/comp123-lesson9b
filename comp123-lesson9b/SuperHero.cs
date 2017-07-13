20a4f72ecd73f33a53c9d3f86f9c36a0cbd4a5dc
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-07-13 13:29:21 -0400

    added superhero class

---
+++ comp123-lesson9b/SuperHero.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Name: Aldrin Fernandez
+ * Date: July 13, 2017
+ * Description: This is superhero class that inherits from a super hero class and implenet the iHasKarma interface
+ * Version: 0.3 - created superhero class
+ */
+namespace comp123_lesson9b
+{
+
+    class SuperHero : SuperHuman, IHasKarma
+    {
+        // pirvate instancce variables or fields
+        private int _karma;
+        //public properties
+
+        public int Karma
+        {
+            get
+            {
+                return this._karma;
+            }
+            set
+            {
+                this._karma = value;
+            }
+        }
+
+        //constructors
+
+
+            /// <summary>
+            /// this is hte main construtor for the super hero class
+            /// </summary>
+            /// <param name="name"></param>
+        public SuperHero(string name)
+            : base(name)
+        {
+
+        }
+
+        //privaet methods
+
+        //publ2ic methods
+    }
+}
