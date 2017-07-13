5ac4903e006ffc381a4cb5bf6279d20529fd9369
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-07-13 14:03:53 -0400

    modified program.cs

--- comp123-lesson9b/Program.cs
+++ comp123-lesson9b/Program.cs
@@ -15,6 +15,14 @@ namespace comp123_lesson9b
             superHero.Powers.Add(new Power("Invisibilty", 30));
             Console.WriteLine(superHero.ToString());
             Console.WriteLine("Karam: "+superHero.Karma);
+
+            SuperVillan superVillain = new SuperVillan("Super Bad Dude", 30);
+            superVillain.Powers.Add(new Power("Flight", 60));
+            superVillain.Powers.Add(new Power("Telekinisis", 30));
+            superVillain.Powers.Add(new Power("Mind Control",20));
+            Console.WriteLine(superVillain.ToString());
+            Console.WriteLine("Malice: " + superVillain.Malice);
+
         }
     }
 }
